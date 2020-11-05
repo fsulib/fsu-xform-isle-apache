1a55ff3b7f30e5de020137ffcb837ad57c1ef2e9
Malcolm Shackelford <mshackelford@fsu.edu>
2020-11-05 12:21:25 -0500

    Flipped the script on it.

--- build_tag_push.sh
+++ build_tag_push.sh
@@ -18,4 +18,7 @@ echo "Tagging image..."
 docker tag $IMAGE_REPO_NAME:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
 echo "Pushing image..."
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
-
+echo "Creating alternate tag..."
+alt_tag="${image_target}-latest"
+docker tag $IMAGE_REPO_NAME:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:${alt_tag}
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:${alt_tag}
